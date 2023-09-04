ea595b2a8c02647db8af937c55024ad8e40c8575
FalinGeorgiy <ivan.ivan.local@mail.ru>
2023-09-04 23:47:02 +0500

    Task1

---
+++ Tyuiu.FalinGS.SprintO.Task1.vo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FalinGS.SprintO.Task1.vo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = "Георгий";
+            int age = 20;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
