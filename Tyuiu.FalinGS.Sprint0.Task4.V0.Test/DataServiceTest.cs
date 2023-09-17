2e9991515db6a222857b11d96019f6af56471db7
FalinGeorgiy <ivan.ivan.local@mail.ru>
2023-09-17 20:03:41 +0500

    Task4

---
+++ Tyuiu.FalinGS.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FalinGS.Sprint0.Task4.V0.Lib;
+
+
+namespace Tyuiu.FalinGS.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(5, DataService.Multiplication(10, 5));
+        }
+
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
